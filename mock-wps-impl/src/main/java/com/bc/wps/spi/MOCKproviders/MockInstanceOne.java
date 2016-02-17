c1d403872ee274f331dd68e6575103a118209a9a
Hans Permana <hans.permana@gmail.com>
2016-02-17 11:33:07 +0100

    Implemented MockWpsOneSpi.java to always return exception.

--- mock-wps-impl/src/main/java/com/bc/wps/spi/MOCKproviders/MockInstanceOne.java
+++ mock-wps-impl/src/main/java/com/bc/wps/spi/MOCKproviders/MockInstanceOne.java
@@ -8,6 +8,8 @@ import com.bc.wps.api.schema.Execute;
 import com.bc.wps.api.schema.ExecuteResponse;
 import com.bc.wps.api.schema.ProcessDescriptionType;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -17,22 +19,23 @@ public class MockInstanceOne implements WpsServiceInstance {
 
     @Override
     public Capabilities getCapabilities(WpsRequestContext context) throws WpsServiceException {
-        return null;
+        throw new WpsServiceException("Unable to perform GetCapabilities operation successfully", new IOException("dummy IOException"));
     }
 
     @Override
     public List<ProcessDescriptionType> describeProcess(WpsRequestContext context, String processId) throws WpsServiceException {
-        return null;
+        throw new WpsServiceException("Unable to perform DescribeProcess operation successfully",
+                                      new IOException("process ID '" + processId + "' is not available"));
     }
 
     @Override
     public ExecuteResponse doExecute(WpsRequestContext context, Execute executeRequest) throws WpsServiceException {
-        return null;
+        throw new WpsServiceException("Unable to perform Execute operation successfully", new JAXBException("dummy JaxbException"));
     }
 
     @Override
     public ExecuteResponse getStatus(WpsRequestContext context, String jobId) throws WpsServiceException {
-        return null;
+        throw new WpsServiceException("Unable to perform GetStatus operation successfully", new IOException("dummy IOException"));
     }
 
     @Override
