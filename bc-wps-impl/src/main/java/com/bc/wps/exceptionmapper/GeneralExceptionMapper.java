4c421429c418b6d658bfd81459a143a63dd2a874
Hans Permana <hans.permana@gmail.com>
2016-03-03 09:39:59 +0100

    Added general exception mapper.

---
+++ bc-wps-impl/src/main/java/com/bc/wps/exceptionmapper/GeneralExceptionMapper.java
@@ -0,0 +1,41 @@
+package com.bc.wps.exceptionmapper;
+
+import com.bc.wps.responses.ExceptionResponse;
+import com.bc.wps.utilities.JaxbHelper;
+import com.bc.wps.utilities.WpsLogger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class maps any NullPointerException to a proper WPS Exception response.
+ *
+ * @author hans
+ */
+@Provider
+public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final Logger LOG = WpsLogger.getLogger();
+
+    @Override
+    public Response toResponse(Exception exception) {
+        LOG.log(Level.SEVERE, "An Exception has been caught.", exception);
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        String exceptionString;
+        try {
+            exceptionString = JaxbHelper.marshal(exceptionResponse.
+                        getGeneralExceptionWithExceptionCauseMessage(exception.getMessage(), exception));
+        } catch (JAXBException jaxbException) {
+            LOG.log(Level.SEVERE, "Unable to marshall the WPS response", exception);
+            ExceptionResponse jaxbExceptionResponse = new ExceptionResponse();
+            exceptionString = jaxbExceptionResponse.getJaxbExceptionResponse();
+        }
+        return Response.serverError()
+                    .entity(exceptionString)
+                    .build();
+    }
+}
